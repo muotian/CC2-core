d58b0123e893c2e63ea87ff90133dd1408bc4424
末天 <56584904+muotian@users.noreply.github.com>
2025-12-03 16:26:27 +0800

    Shader 語法修正

--- orange-rp/assets/orange/shaders/include/util.glsl
+++ orange-rp/assets/orange/shaders/include/util.glsl
@@ -1,7 +1,9 @@
 #version 150
 
 float decodeColor(ivec3 color) {
-  return int(color.r / 16) * 100000 + (color.r % 16) * 10000 \
+  int result = int(color.r / 16) * 100000 + (color.r % 16) * 10000 \
        + int(color.g / 16) * 1000 + (color.g % 16) * 100 \
        + int(color.b / 16) * 10 + (color.b % 16);
+
+  return float(result);
 }
